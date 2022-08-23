d15dac315546e7b5707dc8d2ae37f86cde1823dc
José Gomes <106411184+gomes-tkd@users.noreply.github.com>
2022-08-23 10:30:29 -0300

    Firebase add

---
+++ src/Firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAgydIK35ul8MNtFFcaDkMvfTJnRdPBsFo",
+    authDomain: "fog-tracker.firebaseapp.com",
+    projectId: "fog-tracker",
+    storageBucket: "fog-tracker.appspot.com",
+    messagingSenderId: "997079302557",
+    appId: "1:997079302557:web:39445239f281a14aeb2f74"
+};
+
+const app = initializeApp(firebaseConfig);
