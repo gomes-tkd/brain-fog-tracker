7e9ecaad40b3b3a311ed4b177c549d78bd3b7a91
José Gomes <106411184+gomes-tkd@users.noreply.github.com>
2022-08-31 11:38:18 -0300

    BFT-17-edit-symptom editando os sintomas

--- src/components/SymptomsList.js
+++ src/components/SymptomsList.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Button, Col, Container, Row } from "reactstrap";
+import { Col, Container, Row } from "reactstrap";
 import DeleteSymptomButton from "./DeleteSymptomButton";
+import SymptomEditButton from "./SymptomEditButton";
 
-const SymptomsList = ({ symp }) => (
+const SymptomsList = ({ symptoms, setSymptoms }) => (
   <Container>
     {
-      symp && symp.map(({id, fogginess, anxiety, headache, fatigue, gut}) => (
+      symptoms && symptoms.map(({id, fogginess, anxiety, headache, fatigue, gut}) => (
         <Row
           key={id}
           className={"shadow p-3 mb-3 bg-white rounded pt-3 pb-3 text-center align-items-center justify-content-center"}
@@ -19,7 +20,7 @@ const SymptomsList = ({ symp }) => (
           <Col></Col>
           <Col></Col>
           <Col>
-            <Button color={'light'} block>Edit</Button>
+            <SymptomEditButton id={id} setSymptoms={setSymptoms} />
           </Col>
           <Col>
             <DeleteSymptomButton />
